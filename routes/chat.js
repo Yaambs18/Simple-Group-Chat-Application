5ebce012acb89a88132fece0bacd45949640c40c
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-04 18:44:47 +0530

    clear the code for group chat messages  with username + msg

--- routes/chat.js
+++ routes/chat.js
@@ -1,30 +1,35 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) =>{
-    let msg = 'No Chats Found';
-    try{
-        msg = fs.readFileSync('message.txt', {encoding: "utf-8"});
-    }
-    catch(err){
-        console.log(err);
-    }
-    console.log(msg);
-    res.send(`${msg}<br><form action="/" method="POST"><input id="message" type="text" name="message"><button type="submit">Send</button></form>`);
+router.get("/", (req, res, next) => {
+  let msg = "No Chats Found";
+  try {
+    msg = fs.readFileSync("message.txt", { encoding: "utf-8" });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(msg);
+  res.send(
+    `${msg}<br>
+    <form onsubmit="document.getElementById('username').value=localStorage.getItem('username')" action="/" method="POST">
+    <input id="message" type="text" name="message">
+    <input id="username" type="hidden" name="username">
+    <button type="submit">Send</button>
+    </form>`
+  );
 });
 
-router.post('/', (req, res, next) => {
-    console.log(req.body);
-    fs.appendFileSync('message.txt', req.body['message'], err => {
-        if(err){
-            console.log(err);
-            res.redirect('/404');
-        }
-    });
-    res.redirect('/');
-    // console.log(localStorage.getItem('username'));
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+  fs.appendFileSync("message.txt", `${req.body['username']} : ${req.body["message"]} `, (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/404");
+    }
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
